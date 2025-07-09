b4940382ec796bd402c86513fe008d3ef1243cd1
Brandon Beierle <bbeierle21@gmail.com>
2025-07-09 20:14:56 +0000

    (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' diff

--- src/__tests__/random.test.ts
+++ src/__tests__/random.test.ts
@@ -1,7 +1,7 @@
 import { rng, setSeed } from '../utils/random';
 
 describe('random utility', () => {
-  test("mulberry32 is deterministic", () => {
+  test('mulberry32 is deterministic', () => {
     setSeed(123);
     const seq1 = Array.from({ length: 5 }, rng);
     setSeed(123);
