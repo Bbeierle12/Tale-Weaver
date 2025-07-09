4c8d0718875bbbab14ac48269dd58607315cfe2f
Brandon Beierle <bbeierle21@gmail.com>
2025-07-09 20:18:57 +0000

    (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' diff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type {Metadata} from 'next';
-import { Inter } from 'next/font/google'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
-})
+});
 
 export const metadata: Metadata = {
   title: 'Ecosystem Simulator',
